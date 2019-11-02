ba368c1b9521a2ba47dd3759a5ea81d075e62959
张树森 <1085383404@qq.com>
2019-11-02 22:27:15 +0800

    登录流程和演练

--- app.js
+++ app.js
@@ -1,39 +1,71 @@
 //app.js
 App({
+  //对象:小程序
+  globalData: {
+    token: ''
+  },
   onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
+    //1.先从缓存中取出token
+    const token = wx.getStorageSync('token')
+    //判断token是否有值
+    if(token &&token.length !==0 ){//
+      this.check_token(token)//验证token是否过期
+    }else {//没有token,进行登录操作
+      this.login()
+    }
+   
+  },
+  check_token(token) {
+    console.log('token操作')
+    wx.request({
+      url: 'http://123.207.32.32:3000/auth',
+      method: 'post',
+      header: {
+        token
+      },
+      success:(res)=>{
+        console.log(res)
+        if(!res.data.errCode){
+          this.globalData.token = token;
+        }else {
+          this.login()
         }
+      },
+      fail: function(err){
+        console.log(err)
       }
     })
   },
-  globalData: {
-    userInfo: null
+  login() {
+    console.log('login操作')
+    // 登录操作
+    wx.login({
+      //code只有5分钟的有效期
+      success: res => {
+        console.log(res)
+        // 1.获取code
+        const code = res.code;
+        //2.将code发送给服务器接口
+        wx.request({
+          url: 'http://123.207.32.32:3000/login',
+          method: 'POST',
+          data: {
+            code: code
+          },
+          success: (res) => {
+            console.log(res)
+            const token = res.data.token;
+            //将token保存在globalData中
+            this.globalData.token = token;
+
+            //进行本地存储
+            wx.setStorageSync("token", token)
+
+          }
+        })
+
+      }
+    })
   }
+   
 })
